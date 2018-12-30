e501843e9891cb6d72d9b5ac0fbdff4924bb16b3
chen <chenhaixv1993@outlook.com>
2018-12-30 16:51:53 +0800

    Java注释

---
+++ Java基础学习/src/Java注释.java
@@ -0,0 +1,17 @@
+public class Java注释 {
+    //单行注释
+
+    /*
+    多行注释
+
+     */
+
+    /**
+     *
+     * 文档注释
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
